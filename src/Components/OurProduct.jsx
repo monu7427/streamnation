b0d864cc4d002a8895d9fa095aeee52176474ad5
Manoj Sharma <monusharma078607@gmail.com>
2024-05-09 17:34:38 +0530

    rechange

--- src/Components/OurProduct.jsx
+++ src/Components/OurProduct.jsx
@@ -154,11 +154,12 @@ export const products = [
   // Add more product objects here
 ];
 
+
 const OurProduct = () => {
   return (
     <section className="container mx-auto py-16 mb-4">
       <h2 className="text-3xl font-bold mb-8 bg-black text-white p-9 flex align-center justify-center">Our Services</h2>
-      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-3 m-14">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-3 m-14">
         {products.map(product => (
           <Link to={`/product/${product.id}`} key={product.id} className="text-black">
             <div className="border rounded p-4 cursor-pointer">
